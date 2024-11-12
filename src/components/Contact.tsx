ea30fa66734ae099754866203f0cbeb8bcdffed5
ankitmaurya <ankitmaurya2989@gmail.com>
2024-11-12 22:18:44 +0530

    add phone number

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
@@ -13,9 +13,8 @@ const Contact = () => {
           <span className="text-secondary">better.</span>
         </h3>
       </div>
-      <div className=" w-full h-full lg:flex lg:items-center space-y-5 gap-12">
-        {/* left block */}
-
+      <div className="w-full h-full lg:flex lg:items-center space-y-5 gap-12">
+        {/* Left block */}
         <motion.div
           initial={{ opacity: 0, x: -80 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -33,8 +32,7 @@ const Contact = () => {
           />
         </motion.div>
 
-        {/* right block */}
-
+        {/* Right block */}
         <motion.div
           initial={{ opacity: 0, x: 80 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -42,7 +40,7 @@ const Contact = () => {
             duration: 1,
             ease: [0, 0.71, 0.2, 1.01],
           }}
-          className="right w-full space-y-5 tracking-wide "
+          className="right w-full space-y-5 tracking-wide"
         >
           <p className="text-gray-400 leading-8 md:leading-10">
             Thank you for visiting! This is your direct line to reach me.
@@ -50,11 +48,13 @@ const Contact = () => {
             development, or just want to say hello, I&apos;m here and eager to
             connect with you.
           </p>
+
+          {/* Email link */}
           <div className="email-link space-y-5">
             <p className="text-gray-400">Reach out to me via email</p>
             <p>
               <a
-                href="mailto:ankit.fsdev.com"
+                href="mailto:ankit.fsdev@gmail.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-200 text-xl flex items-center underline hover:text-secondary duration-200"
@@ -64,6 +64,22 @@ const Contact = () => {
               </a>
             </p>
           </div>
+
+          {/* Phone link */}
+          <div className="phone-link space-y-5">
+            <p className="text-gray-400">Reach out to me via phone</p>
+            <p>
+              <a
+                href="tel:+911234567891"
+                className="text-gray-200 text-xl flex items-center underline hover:text-secondary duration-200"
+              >
+                <PhoneIcon className="h-8 w-8 mr-5 text-secondary" />
+                +91 8010078449
+              </a>
+            </p>
+          </div>
+
+          {/* Social media links */}
           <div className="social-media space-y-5 pt-6">
             <p className="text-gray-400">Connect through social media</p>
             <div className="flex flex-wrap gap-5 justify-between max-w-[600px]">
@@ -92,14 +108,14 @@ const Contact = () => {
                     src="/twitter.svg"
                     width={32}
                     height={32}
-                    alt="ankit-maurya_twitter"
+                    alt="ankit-maurya-twitter"
                   />
                   Twitter
                 </a>
               </p>
               <p>
                 <a
-                  href="www.linkedin.com/in/ankitmaurya-byte"
+                  href="https://www.linkedin.com/in/ankitmaurya-byte"
                   className="text-gray-200 flex items-center gap-3 hover:scale-105 duration-200"
                   target="_blank"
                 >
@@ -107,7 +123,7 @@ const Contact = () => {
                     src="/linkedin.svg"
                     width={32}
                     height={32}
-                    alt="ankit-mauryaweb_linkedin"
+                    alt="ankit-maurya-linkedin"
                   />
                   LinkedIn
                 </a>
@@ -122,7 +138,7 @@ const Contact = () => {
                     src="/discord.svg"
                     width={32}
                     height={32}
-                    alt="ankitm-maurya-discord"
+                    alt="ankit-maurya-discord"
                   />
                   Discord
                 </a>
